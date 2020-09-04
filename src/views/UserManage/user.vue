084a5a7847de2713e0eb7d2f5cb3526182089405
carey <btestzzw@163.com>
2020-09-04 18:27:01 +0800

    完成user页面编写

--- src/views/UserManage/user.vue
+++ src/views/UserManage/user.vue
@@ -3,18 +3,18 @@
         <el-dialog :title="operateType === 'add' ? '新增用户': '修改用户'" :visible.sync="isShow">
             <common-form :form="operateForm" :formLabel="operateLabel"></common-form>
             <span slot="footer" class="dialog-footer">
-                <el-button @click="dialogFormVisible = false">取消</el-button>
-                <el-button type="primary" @click="dialogFormVisible = false">确认</el-button>
+                <el-button @click="isShow=false">取消</el-button>
+                <el-button type="primary" @click="submit">确认</el-button>
             </span>
         </el-dialog>
         <div class="manage-header">
-            <el-button type="primary">+新增</el-button>
+            <el-button type="primary" @click="newUser">+新增</el-button>
             <common-form :form="searchForm" :formLabel="formLabel" inline>
-                <el-button type="primary">搜索</el-button>
+                <el-button type="primary" @click="getList(searchForm.keyword)">搜索</el-button>
             </common-form>
         </div>
         <common-table :tableData="tableData" :tableLabel="tableLabel" :config="config" @changePage="getList"
-                      @edit="editUser"></common-table>
+                      @edit="editUser" @delete="delUser"></common-table>
     </div>
 </template>
 
@@ -116,14 +116,15 @@
             this.getList();
         },
         methods: {
-            getList() {
+            // 取回所有的用户信息
+            getList(name='') {
                 this.config.loading = true;
                 this.$http.get("/api/user/getUser", {
                     params: {
                         page: this.config.page,
+                        name
                     }
                 }).then(res => {
-                    console.log(res.data);
                     this.tableData = res.data.list.map(item => {
                         item.sexLabel = item.sex === 0 ? "女" : "男";
                         return item;
@@ -132,11 +133,44 @@
                     this.config.loading = false;
                 })
             },
+            newUser(){
+                this.isShow = true;
+                this.operateType = 'add';
+            },
             editUser(row) {
                 this.operateType = 'edit';
                 this.isShow = true;
                 this.operateForm = row;
-                console.log(row)
+            },
+            delUser(row){
+                this.$confirm('将会删除用户信息, 确认删除?', '提示', {
+                    confirmButtonText: '确认',
+                    cancelButtonText: '取消',
+                    type: 'warning'
+                }).then(() => {
+                    this.$http.get('/user/del', {
+                        params: {
+                            id: row.id
+                        }
+                    }).then(res => {
+                        this.$message({
+                            type: 'success',
+                            message: res.data.message
+                        });
+                    });
+                    this.getList();
+                }).catch(() => {
+                    this.$message({
+                        type: 'info',
+                        message: '已取消删除'
+                    });
+                });
+            },
+            submit(){
+                this.isShow = false;
+                this.operateType === 'edit' ? this.$http.post('/user/edit', this.operateForm) :
+                    this.$http.post('/user/add', this.operateForm);
+                this.getList();
             }
         }
     }
